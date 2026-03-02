03b058931087754b98a43a4d09d541d02e26bd29
Mirna El Tohamy <m.tohamy.2000@gmail.com>
2026-03-03 00:28:57 +0200

    p1q2

--- OOPAssignment5/Program.cs
+++ OOPAssignment5/Program.cs
@@ -3,3 +3,28 @@
 //Interfaces are used instead of concrete classes to achieve loose coupling through abstraction which allows more flexibility , easy testing & maintainability
 //achieve multiple inheritence - loose coupling - abstraction to define capability not implementation - polymorphism
 #endregion
+
+#region p1q2
+//a
+//same method name conflict - class currently provides same implementation for both 
+//b
+//Fix by explicit implementation - call through interface name
+//class Translator : IEnglishSpeaker, IArabicSpeaker
+//{
+//    void IEnglishSpeaker.Greet()
+//    {
+//        Console.WriteLine("Hello");
+//    }
+//    void IArabicSpeaker.Greet()
+//    {
+//        Console.WriteLine("Ahlan");
+//    }
+//}
+//c
+//Greet() can only be called through interface reference pointing to Translator object only
+//IEnglishSpeaker tEnglish = new Translator();
+//IArabicSpeaker tArabic = new translator();
+//tEnglish.Greet();
+//tArabic.Greet();
+#endregion
+
