045b180ddc4a372f2a8485aceaab95284ab4b21a
Mirna El Tohamy <m.tohamy.2000@gmail.com>
2026-03-03 00:13:48 +0200

    p1q1

--- OOPAssignment5/Program.cs
+++ OOPAssignment5/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿#region p1q1
+//Interface defines a contract that a class must implement. It defines what a class does but not how it does it.
+//Interfaces are used instead of concrete classes to achieve loose coupling through abstraction which allows more flexibility , easy testing & maintainability
+//achieve multiple inheritence - loose coupling - abstraction to define capability not implementation - polymorphism
+#endregion
